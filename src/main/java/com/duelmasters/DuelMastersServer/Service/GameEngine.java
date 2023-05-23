36e1cee50240442a6c1f034839839de9a5fcff63
MarcusBanea <marcus.banea27@yahoo.com>
2023-05-23 20:23:44 +0300

    Added added-power logic to attack event; added more abilities to ability dictionary

--- src/main/java/com/duelmasters/DuelMastersServer/Service/GameEngine.java
+++ src/main/java/com/duelmasters/DuelMastersServer/Service/GameEngine.java
@@ -1,6 +1,7 @@
 package com.duelmasters.DuelMastersServer.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
@@ -46,11 +47,52 @@ public class GameEngine {
 		}
 	}
 	
-	public void moveCard(String playerIndicator, String zoneFrom, String zoneTo, int index) {		
+	public Aftermath moveCard(String playerIndicator, String zoneFrom, String zoneTo, int index) {	
+		
+		Aftermath aftermath = null;
+		
 		GameCardDTO card = getCardInZone(playerIndicator, zoneFrom, index);
+		
+		//check if there is a placement ability triggered for current card
+		//at this moment, only battleZone-placement abilities exist
+		//also, cards can have only 1 placement abilities
+		if(zoneFrom.equals("hand") && zoneTo.equals("battleZone")) {
+			if(card.getPlacement().length != 0) {
+				aftermath = new Aftermath();
+				aftermath.addAbility(playerIndicator + "#" + Abilities.valueOf(card.getPlacement()[0]).getAbility());
+			}
+		}
+		
 		addCardInZone(playerIndicator, zoneTo, card);
 		removeCardInZone(playerIndicator, zoneFrom, index);
 		
+		return aftermath;
+		
+	}
+	
+	public ArrayList<GameCardDTO> getCardsInZone(String playerIndicator, String zone) {
+		PlayerDTO player = playerIndicator.equals("player1") ? this.player1 : this.player2;
+		
+		switch(zone) {
+			case "battleZone" : {
+				return player.getBattleZone();
+			}
+			case "manaZone" : {
+				return player.getManaZone();
+			}
+			case "hand" : {
+				return player.getHand();
+			}
+			case "shields" : {
+				return player.getShields();
+			}
+			case "graveyard" : {
+				return player.getGraveyard();
+			}
+			default : {
+				return null;
+			}
+		}
 	}
 	
 	public GameCardDTO getCardInZone(String playerIndicator, String zone, int index) {
@@ -76,6 +118,10 @@ public class GameEngine {
 			}
 			case "graveyard" : {
 				card = player.getGraveyard().get(index);
+				break;
+			}
+			case "deck" : {
+				card = player.getDeck().get(index);
 				break;
 			}
 			default : {
@@ -107,6 +153,10 @@ public class GameEngine {
 			}
 			case "graveyard" : {
 				player.getGraveyard().remove(index);
+				break;
+			}
+			case "deck" : {
+				player.getDeck().remove(index);
 				break;
 			}
 			default : {
@@ -144,6 +194,96 @@ public class GameEngine {
 			}
 		}
 	}
+	
+	public ArrayList<String> getAttackOptionsForCard(String player, String zone, int index) {
+		GameCardDTO card = getCardInZone(player, zone, index);
+		
+		ArrayList<String> attackOptions = new ArrayList<>();
+		
+		String opponentIndicator = player.equals("player1") ? "player2" : "player1";
+		
+		ArrayList<GameCardDTO> opponentShields = getCardsInZone(opponentIndicator, "shields");
+		ArrayList<GameCardDTO> opponentBattleZone = getCardsInZone(opponentIndicator, "battleZone");
+		
+		String[] attackProperty = card.getAttackProperty();
+		for(String property : attackProperty) {
+			//check "This creature can't attack" property
+			if(property.indexOf("CNA") == 0) {
+				String encodedProperty = Abilities.valueOf(property).getAbility();
+				switch(encodedProperty) {
+					case "CNA_0" : {
+						attackOptions.add("NONE");
+						break;
+					}
+					case "CNA_1" : {
+						for(int i = 0; i < opponentBattleZone.size(); i++) {
+							if(canCardBeAttacked(card, opponentBattleZone.get(i))) {
+								attackOptions.add("battleZone_" + i);
+							}	
+						}
+						break;
+					}
+					case "CNA_2" : {
+						for(int i = 0; i < opponentShields.size(); i++) {
+							attackOptions.add("shields_" + i);
+						}
+						break;
+					}
+					default : {
+						break;
+					}
+				}
+			}
+			//check "This creature can attack" property
+			//TODO
+			if(property.indexOf("CA") == 0) {
+				String encodedProperty = Abilities.valueOf(property).getAbility();
+			}
+		}
+		if(attackOptions.size() == 0) {
+			attackOptions.add("ALL");
+		}
+		
+		return attackOptions;
+	}
+	
+	private boolean canCardBeAttacked(GameCardDTO attackingCard, GameCardDTO attackedCard) {
+		//check "This creature can't be attacked" property
+		boolean canBeAttacked = true;
+		
+		for(String property : attackedCard.getAttackProperty()) {
+			if(property.indexOf("CBA") == 0) {
+				String[] encodedProperty = Abilities.valueOf(property).getAbility().split("_");
+				if(encodedProperty[0].equals("0")) {
+					canBeAttacked = false;
+				}
+				else {
+					if(encodedProperty[2].equals("1")) {
+						//check card realm
+						String[] realms = encodedProperty[2].split("/");
+						for(String realm : realms) {
+							if(realm.equals(attackingCard.getRealm())) {
+								canBeAttacked = false;
+							}
+						}
+					}
+					else if(encodedProperty[2].equals("2")) {
+						//check card class
+						String[] classes = encodedProperty[2].split("/");
+						for(String cardClass : classes) {
+							//the class filter may represent the whole class like Mecha del Sol
+							//or parts of the class, like Dragon
+							if(cardClass.indexOf(attackingCard.getCardClass()) != -1) {
+								canBeAttacked = false;
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		return canBeAttacked;
+	}
 		
 	public Aftermath execute2CardTypeAction(ActionCard actionCard1, ActionCard actionCard2, String actionType) {
 		Aftermath aftermath = new Aftermath();
@@ -154,25 +294,53 @@ public class GameEngine {
 		switch(actionType) {
 			case "block" : 
 			case "attack" : {
-				//check for trigger on attacking card
+				
+				//check if the defending player has at least one blocker
+				//in that case, fill only the "extras" attribute of the aftermath object and return it
 				//TODO
-				for(Ability ability : card1.getAbility()) {
-					if(ability.getTrigger().equals("attacking")) {
-						//check for "Whenever this creature attacks" ability
-						String abilityEffectEncoded = ability.getEffect();
-						String abilityEffectDecoded = "";
-						if(abilityEffectEncoded.indexOf("WTCA") != -1) {
-							abilityEffectDecoded = abilityEffectEncoded.substring(abilityEffectEncoded.indexOf("WTCA"), 6);
-							abilityEffectDecoded = Abilities.valueOf(abilityEffectDecoded).getAbility();
-						}
-						aftermath.addAbility(abilityEffectDecoded);
+//				if(actionType.equals("attack")) {
+//					for(GameCardDTO card : getPlayer(actionCard2.getPlayer()).getBattleZone()) {
+//						
+//					}
+//					aftermath.setExtras("");
+//				}
+				
+				//check for trigger on attack
+				for(String attackAbility : card1.getAttackProperty()) {
+					if(attackAbility.indexOf("WTCA") != -1) {
+						aftermath.addAbility(actionCard1.getPlayer() + "#" + Abilities.valueOf(attackAbility).getAbility());
 					}
+				}
+				
+				//get cards power level (when attacking)
+				int attackingCardPower = 0;
+				int defendingCardPower = 0;
+				if(card1.getPower().indexOf("+") != -1) {
+					attackingCardPower = Integer.parseInt(card1.getPower().substring(0, card1.getPower().length() - 1));
+					//check for "PA" attack property
+					for(String attackProp : card1.getAttackProperty()) {
+						if(attackProp.startsWith("PA")) {
+							attackingCardPower += Integer.parseInt(Abilities.valueOf(attackProp).getAbility());
+						}
+					}
+					attackingCardPower += getAddedPowerForCard(card1, false);
+				}
+				else {
+					attackingCardPower = Integer.parseInt(card1.getPower());
+				}
+				
+				if(card2.getPower().indexOf("+") != -1) {
+					defendingCardPower = Integer.parseInt(card2.getPower().substring(0, card2.getPower().length() - 1));
+					defendingCardPower += getAddedPowerForCard(card2, true);
+				}
+				else {
+					defendingCardPower = Integer.parseInt(card2.getPower());
 				}
 				
 				//execute action attack
 				//move cards to appropriate zones and modify their states (power level, speed attacker, ...)
 				//check for triggered abilities after the execution
-				if(card1.getPower() > card2.getPower()) {
+				if(attackingCardPower > defendingCardPower) {
 					
 					//card2 will be destroyed
 					moveCard(actionCard2.getPlayer(), "battleZone", "graveyard", actionCard2.getIndex());
@@ -180,10 +348,8 @@ public class GameEngine {
 					aftermath.setCard1State("");
 					aftermath.setCard2State("destroyed");
 					
-					//check for trigger on destroyed card
-					//TODO
 				}
-				else if(card1.getPower() < card2.getPower()) {
+				else if(attackingCardPower < defendingCardPower) {
 					
 					aftermath.setCard1State("destroyed");
 					aftermath.setCard2State("");
@@ -201,6 +367,23 @@ public class GameEngine {
 					moveCard(actionCard2.getPlayer(), "battleZone", "graveyard", actionCard2.getIndex());
 				}
 				
+				//check for trigger on destroyed card
+				if(aftermath.getCard1State().equals("destroyed")) {
+					for(String attackAbility : card1.getAttackProperty()) {
+						if(attackAbility.indexOf("WTCID") != -1) {
+							aftermath.addAbility(actionCard1.getPlayer() + "#" + Abilities.valueOf(attackAbility).getAbility());
+						}
+					}
+				}
+				
+				if(aftermath.getCard2State().equals("destroyed")) {
+					for(String attackAbility : card2.getAttackProperty()) {
+						if(attackAbility.indexOf("WTCID") != -1) {
+							aftermath.addAbility(actionCard2.getPlayer() + "#" + Abilities.valueOf(attackAbility).getAbility());
+						}
+					}
+				}
+				
 				break;
 			}
 			default : {
@@ -212,6 +395,46 @@ public class GameEngine {
 		return aftermath;
 	}
 	
+	private int getAddedPowerForCard(GameCardDTO card, boolean noAttackingAddedPower) {
+		int finalAddedPower = 0;
+		for(String addedPowerProp : card.getAddedPower()) {
+			//ATCG ability will not be checked, as this card is not attacking
+			if(addedPowerProp.startsWith("TCG") || addedPowerProp.startsWith("ATCG")) {
+				if(addedPowerProp.startsWith("ATCG") && noAttackingAddedPower == true) {
+					break;
+				}
+				//decode TCG ability
+				//the encoded ability has 6 parts:
+				//0 - power level to be added
+				//1 - type condition (NUL - for each card of type...; ONLY - if all cards have the same type...
+				//2 - count condition (ALL - count all cards of type...; ONE - check if there is at least 1 card of type...)
+				//3 - filter code (0 - no filter; 1 - realm filter; ...)
+				//4 - filter property (if filter code is 1, then the filter property will indicate which realm will be used to filter
+				//5 - zone (HD - hand, SD - shields, ...)
+				String encodedAbility = Abilities.valueOf(addedPowerProp).getAbility();
+				String[] encodedAblilityParts = encodedAbility.split("_");
+				//get power level
+				int addedPowerLevel = Integer.parseInt(encodedAblilityParts[0].substring(3));
+				//count cards with filter in zone
+				int nrOfCardsInZoneWithFilter = count(Integer.parseInt(encodedAblilityParts[3]),
+						encodedAblilityParts[4], encodedAblilityParts[5]);
+				//check the combination of type and count conditions
+				//compute the final added power
+				if(encodedAblilityParts[1].equals("NUL") && encodedAblilityParts[2].equals("ALL")) {
+					finalAddedPower = addedPowerLevel * nrOfCardsInZoneWithFilter;
+				}
+				else if(encodedAblilityParts[1].equals("NUL") && encodedAblilityParts[2].equals("ONE")) {
+					finalAddedPower = addedPowerLevel;
+				}
+				else if(encodedAblilityParts[1].equals("ONLY") && encodedAblilityParts[2].equals("ALL")) {
+					if(nrOfCardsInZoneWithFilter == count(0, null, encodedAblilityParts[5])) {
+						finalAddedPower = addedPowerLevel;
+					}
+				}
+			}
+		}
+		return finalAddedPower;
+	}
 	
 	// utils
 
@@ -226,7 +449,7 @@ public class GameEngine {
 		}
 		// realm
 		case 1: {
-			return selectedZone.stream().filter(card -> card.getCardRealm().equals(filter)).collect(Collectors.toList())
+			return selectedZone.stream().filter(card -> card.getRealm().equals(filter)).collect(Collectors.toList())
 					.size();
 		}
 		// class
@@ -236,17 +459,17 @@ public class GameEngine {
 		}
 		// less power
 		case 3: {
-			return selectedZone.stream().filter(card -> card.getPower() > Integer.parseInt(filter))
+			return selectedZone.stream().filter(card -> Integer.parseInt(card.getPower()) > Integer.parseInt(filter))
 					.collect(Collectors.toList()).size();
 		}
 		// more power
 		case 4: {
-			return selectedZone.stream().filter(card -> card.getPower() < Integer.parseInt(filter))
+			return selectedZone.stream().filter(card -> Integer.parseInt(card.getPower()) < Integer.parseInt(filter))
 					.collect(Collectors.toList()).size();
 		}
 		// power
 		case 5: {
-			return selectedZone.stream().filter(card -> card.getPower() == Integer.parseInt(filter))
+			return selectedZone.stream().filter(card -> Integer.parseInt(card.getPower()) == Integer.parseInt(filter))
 					.collect(Collectors.toList()).size();
 		}
 		// blocker TODO
@@ -297,7 +520,7 @@ public class GameEngine {
 		// realm
 		case 1: {
 			for(int i = 0; i < selectedZone.size(); i++) {
-				if(selectedZone.get(i).getCardRealm().equals(filter)) {
+				if(selectedZone.get(i).getRealm().equals(filter)) {
 					selectedCards.add(i);
 				}
 			}
@@ -315,7 +538,7 @@ public class GameEngine {
 		// less power
 		case 3: {
 			for(int i = 0; i < selectedZone.size(); i++) {
-				if(selectedZone.get(i).getPower() > Integer.parseInt(filter)) {
+				if(Integer.parseInt(selectedZone.get(i).getPower()) > Integer.parseInt(filter)) {
 					selectedCards.add(i);
 				}
 			}
@@ -324,7 +547,7 @@ public class GameEngine {
 		// more power
 		case 4: {
 			for(int i = 0; i < selectedZone.size(); i++) {
-				if(selectedZone.get(i).getPower() < Integer.parseInt(filter)) {
+				if(Integer.parseInt(selectedZone.get(i).getPower()) < Integer.parseInt(filter)) {
 					selectedCards.add(i);
 				}
 			}
@@ -333,7 +556,7 @@ public class GameEngine {
 		// power
 		case 5: {
 			for(int i = 0; i < selectedZone.size(); i++) {
-				if(selectedZone.get(i).getPower() == Integer.parseInt(filter)) {
+				if(Integer.parseInt(selectedZone.get(i).getPower()) == Integer.parseInt(filter)) {
 					selectedCards.add(i);
 				}
 			}
@@ -393,17 +616,68 @@ public class GameEngine {
 		case "BZ0": {
 			return player1.getBattleZone();
 		}
+		case "BZ1": {
+			return player2.getBattleZone();
+		}
+		case "BZ2": {
+			 ArrayList<GameCardDTO> cards = player1.getBattleZone();
+			 cards.addAll(player2.getBattleZone());
+			 return cards;
+		}
 		case "MN0": {
 			return player1.getManaZone();
+		}
+		case "MN1": {
+			return player2.getManaZone();
+		}
+		case "MN2": {
+			 ArrayList<GameCardDTO> cards = player1.getManaZone();
+			 cards.addAll(player2.getManaZone());
+			 return cards;
 		}
 		case "GV0": {
 			return player1.getGraveyard();
 		}
+		case "GV1": {
+			return player2.getGraveyard();
+		}
+		case "GV2": {
+			 ArrayList<GameCardDTO> cards = player1.getGraveyard();
+			 cards.addAll(player2.getGraveyard());
+			 return cards;
+		}
 		case "DK0": {
 			return player1.getDeck();
 		}
+		case "DK1": {
+			return player2.getDeck();
+		}
+		case "DK2": {
+			 ArrayList<GameCardDTO> cards = player1.getDeck();
+			 cards.addAll(player2.getDeck());
+			 return cards;
+		}
 		case "HD0": {
 			return player1.getHand();
+		}
+		case "HD1": {
+			return player2.getHand();
+		}
+		case "HD2": {
+			 ArrayList<GameCardDTO> cards = player1.getHand();
+			 cards.addAll(player2.getHand());
+			 return cards;
+		}
+		case "SD0": {
+			return player1.getShields();
+		}
+		case "SD1": {
+			return player2.getShields();
+		}
+		case "SD2": {
+			 ArrayList<GameCardDTO> cards = player1.getShields();
+			 cards.addAll(player2.getShields());
+			 return cards;
 		}
 		default: {
 			return null;
